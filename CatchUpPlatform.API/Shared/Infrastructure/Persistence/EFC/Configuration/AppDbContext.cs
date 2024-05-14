d2b99aef8ce9c99da0a7fd8621522b5c7615923f
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-14 15:16:40 -0500

    chore: added base repository.

---
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,26 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<FavoriteSource>().ToTable("FavoriteSources");
+        builder.Entity<FavoriteSource>().HasKey(f => f.Id);
+        builder.Entity<FavoriteSource>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<FavoriteSource>().Property(f => f.NewsApiKey).IsRequired();
+        builder.Entity<FavoriteSource>().Property(f => f.SourceId).IsRequired();
+        
+        builder.UseSnakeCaseNamingConvention();
+    }
+}
--- CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Repositories/BaseRepository.cs
@@ -1,31 +1,40 @@
 using CatchUpPlatform.API.Shared.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
 public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
 {
-    public Task AddAsync(TEntity entity)
+    protected readonly AppDbContext Context;
+
+    protected BaseRepository(AppDbContext context)
     {
-        throw new NotImplementedException();
+        Context = context;
     }
 
-    public Task<TEntity> FindByIdAsync(int id)
+    public async Task AddAsync(TEntity entity)
     {
-        throw new NotImplementedException();
+        await Context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public async Task<TEntity> FindByIdAsync(int id)
+    {
+        return await Context.Set<TEntity>().FindAsync(id);
     }
 
     public void Update(TEntity entity)
     {
-        throw new NotImplementedException();
+        Context.Set<TEntity>().Update(entity);
     }
 
     public void Remove(TEntity entity)
     {
-        throw new NotImplementedException();
+        Context.Set<TEntity>().Remove(entity);
     }
 
-    public Task<IEnumerable<TEntity>> ListAsync()
+    public async Task<IEnumerable<TEntity>> ListAsync()
     {
-        throw new NotImplementedException();
+        return await Context.Set<TEntity>().ToListAsync();
     }
 }
