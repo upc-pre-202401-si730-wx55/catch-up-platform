ee32237bb99f864f81123b7523f4d8127d5ae096
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-17 07:25:26 -0500

    feat: added query and query handlers for favorite source aggregate.

---
+++ CatchUpPlatform.API/News/Application/Internal/QueryServices/FavoriteSourceQueryService.cs
@@ -0,0 +1,25 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Domain.Model.Queries;
+using CatchUpPlatform.API.News.Domain.Repositories;
+using CatchUpPlatform.API.News.Domain.Services;
+
+namespace CatchUpPlatform.API.News.Application.Internal.QueryServices;
+
+public class FavoriteSourceQueryService(IFavoriteSourceRepository favoriteSourceRepository)
+    : IFavoriteSourceQueryService
+{
+    public async Task<FavoriteSource> Handle(GetFavoriteSourceByIdQuery query)
+    {
+        return await favoriteSourceRepository.FindByIdAsync(query.Id);
+    }
+
+    public async Task<FavoriteSource> Handle(GetFavoriteSourceByNewsApiKeyAndSourceIdQuery query)
+    {
+        return await favoriteSourceRepository.FindByNewsApiKeyAndSourceIdAsync(query.NewsApiKey, query.SourceId);
+    }
+
+    public async Task<IEnumerable<FavoriteSource>> Handle(GetAllFavoriteSourcesByNewsApiKeyQuery query)
+    {
+        return await favoriteSourceRepository.FindAllByNewsApiKeyAsync(query.NewsApiKey);
+    }
+}
